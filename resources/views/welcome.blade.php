f8e7601f3b465c06f0af974a8ada31d393be364a
Adrian <adibu8@gmail.com>
2021-04-29 11:49:58 +0200

    Lab1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,8 +82,8 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
+                    - Laboratorium 1
                 </div>
-
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
