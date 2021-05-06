83b594f163d86ff952f95f489a87a7d6b89031db
Adrian <adibu8@gmail.com>
2021-05-06 12:10:37 +0200

    Lab2

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::post("/login", function(Request $request){
+//     $data = $request->validate([
+//         'email' => 'required',
+//         'password' => 'required'
+//     ]);
+
+//     if(Auth::attempt($request->only('email', 'password'))){
+//         return response()->json('', 204);
+//     }
+//     return response()->json(['error' => 'Nieudane logowanie!'], 401);
+// })
+
+// Route::post('/logout', function(Request $request) {
+//     Auth::logout();
+//     return response('');
+// })
