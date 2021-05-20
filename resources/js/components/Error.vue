ad75ea5743bab08c0fdf5f2cc0557d7979b03899
Adrian <adibu8@gmail.com>
2021-05-20 12:58:57 +0200

    LAB4 Finished

--- resources/js/components/Error.vue
+++ resources/js/components/Error.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="isError">
+    <div v-if="isError" class="myModal">
         <div class="modal-content">
             <h3>Upps, coś poszło nie tak!</h3>
             <h6 class="text-justify">{{ message }}</h6>
@@ -48,4 +48,28 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.myModal {
+    display: flex; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0, 0, 0); /* Fallback color */
+    background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+}
+
+.modal-content {
+    background-color: #cf3535;
+    color: white;
+    font-family: Georgia, "Times New Roman", Times, serif;
+    margin: 15% auto; /* 15% from the top and centered */
+    padding: 20px;
+    border: 1px solid #888;
+    width: 400px;
+    height: 200px;
+}
+</style>
